c951e5b6733175d6cfbe6f6b453f7d988eb158f4
Ion Craciunescu <icraciunescu@hotmail.com>
2019-06-08 12:21:20 +0300

    My BaseRepository

--- booking-core/src/main/java/ro/mxp/booking/core/base/MyBaseRepository.java
+++ booking-core/src/main/java/ro/mxp/booking/core/base/MyBaseRepository.java
@@ -3,10 +3,11 @@ package ro.mxp.booking.core.base;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
 
+import java.io.Serializable;
 import java.util.List;
 
 @NoRepositoryBean
-public interface MyBaseRepository<T, ID extends BaseEntity> extends Repository<T, Integer> {
+public interface MyBaseRepository<T, ID extends Serializable> extends Repository<T, Integer> {
 
     <S extends T> S save(S entity);
     T findOne(ID id);
